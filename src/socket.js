c0247474e9895b437d2838df878eb1d4e758117a
Aditya Kale <kaleaditya.nitin2023@vitstudent.ac.in>
2026-04-15 13:45:27 +0530

    frontend connected to backend

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:3001";
+const SOCKET_URL = "https://smart-parking-system-9fzt.onrender.com";
 export const socket = io(SOCKET_URL);
